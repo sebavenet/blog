71831aa58c22edea05b16f172973fe89b74e1168
Sebastien Avenet <avenet.seb@gmail.com>
2012-07-20 16:29:51 +0200

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'aa782b2a9cec4a7e69b41ba9d49980718e620e22d7ed499820aeac06a05606ce3ef86eb563e041a8834051a0c537e15285e396c1adf371ca6870191874000183'
---
+++ db/migrate/20120720131253_create_tags.rb
@@ -0,0 +1,13 @@
+class CreateTags < ActiveRecord::Migration
+  def change
+    create_table :tags do |t|
+      t.string :name
+      t.references :post
+
+      t.timestamps
+    end
+    add_index :tags, :post_id
+
+    drop_table :themes
+  end
+end
